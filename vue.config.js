36a617359959eb615ff75c826cd88c0caad6911d
Megan Martel <115752040+meganmartel@users.noreply.github.com>
2024-03-06 20:24:27 -0500

    Correction chemin de base

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-      ? '/meganmartel/projet-2-esport/' // Remplacez 'nom-de-votre-projet' par le nom de votre projet sur GitHub Pages
+      ? '/projet-2-esport/' // Remplacez 'nom-de-votre-projet' par le nom de votre projet sur GitHub Pages
       : '/'
   }
   
