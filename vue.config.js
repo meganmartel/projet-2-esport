680d1e77f5caad70d9b2fd1209c238e8c050e76e
Megan Martel <115752040+meganmartel@users.noreply.github.com>
2024-03-06 19:50:47 -0500

    Déploiement

---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/projet-2-esport/' // Remplacez 'nom-de-votre-projet' par le nom de votre projet sur GitHub Pages
+      : '/'
+  }
+  
