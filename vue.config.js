b469238133e588d674ec70293d4e3738c8945ef6
Megan Martel <115752040+meganmartel@users.noreply.github.com>
2024-03-06 20:19:16 -0500

    Correction url pour déploiement

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-      ? '/projet-2-esport/' // Remplacez 'nom-de-votre-projet' par le nom de votre projet sur GitHub Pages
+      ? '/meganmartel/projet-2-esport/' // Remplacez 'nom-de-votre-projet' par le nom de votre projet sur GitHub Pages
       : '/'
   }
   
